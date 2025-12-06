db84d84a583e1086de0972c8dd5da5026355336a
Kelly <kellyguo1101@gmail.com>
2025-12-05 16:30:14 -0800

    started up hotel proj

---
+++ Room.java
@@ -0,0 +1,24 @@
+
+public class Room {
+
+    private int roomNumber;
+    private RoomType roomType;
+    private boolean isAvailable;
+    private double perNight;
+
+    public Room(int roomNumber, RoomType roomType, boolean isAvailable, double perNight) {
+        this.roomNumber = roomNumber;
+        this.roomType = roomType;
+        this.isAvailable = isAvailable;
+        this.perNight = perNight;
+    }
+
+    public int getRoomNumber() {
+        return roomNumber;
+    }
+
+    public double getPricePerNight() {
+        return perNight;
+    }
+
+}
